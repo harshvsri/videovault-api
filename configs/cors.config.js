23caaab3ef29875a6d8eaf8a2ffb4fd3e363bacd
Harsh V Srivastava <harshvsri@gmail.com>
2024-03-16 15:50:55 +0530

    Update cors.config.js
    Add frontend url

--- configs/cors.config.js
+++ configs/cors.config.js
@@ -2,7 +2,7 @@
  * Handles requests from a frontend web application.
  * This file sets up CORS configuration options for the backend server, which ensures that requests are only allowed from specified origins and credentials are properly handled.
  */
-const allowedOrigins = ["http://localhost:3000", "http://localhost:5173"];
+const allowedOrigins = ["https://happy-pebble-08b779700.5.azurestaticapps.net", "http://localhost:3000", "http://localhost:5173"];
 
 const corsOptions = {
   origin: (origin, callback) => {
